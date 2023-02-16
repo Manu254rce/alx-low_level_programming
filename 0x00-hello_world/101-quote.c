c0c889ed4672273a8d33c44da468b6d2aad9b9c3
Manu254rce <straussmoabit@gmail.com>
2023-02-16 13:11:46 -0800

    Printing a code result to the stndard error

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+# include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Evaluate my code to standard error
+ * Return: 1
+ *
+ */
+
+int main(void)
+{
+
+	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	return(1);
+}
