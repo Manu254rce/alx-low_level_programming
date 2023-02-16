2987cb1f6f08516dd28f2df64c39b87098c502c0
Manu254rce <straussmoabit@gmail.com>
2023-02-16 12:19:26 -0800

    Getting started with programming in C

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,10 +8,10 @@
 
 int main(void)
 {
-	printf("Size of a char: %d\n byte(s)", sizeof(char));
-	printf("Size of an int: %u\n byte(s)", sizeof(int));
-	printf("Size of a long int: %lu\n byte(s)", sizeof(long int));
-	printf("Size of a long long int: %u\n byte(s)", sizeof(long long int));
-	printf("Size of a float: %u\n byte(s)", sizeof(float));
+	printf("Size of a char: %d byte(s)\n", sizeof(char));
+	printf("Size of an int: %u byte(s)\n", sizeof(int));
+	printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %u byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %u byte(s)\n", sizeof(float));
 	return (0);
 }
