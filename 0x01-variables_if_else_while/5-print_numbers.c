50d96bd1f3c48dbff5532a414737a1a106cfaf64
Manu254rce <straussmoabit@gmail.com>
2023-02-17 10:10:45 -0800

    C variables and control flow

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+# include <stdio.h>
+# include <stdlib.h>
+
+/**
+ * main - Program to print numbers from 0 to 10
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i = 0;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d", i);
+	}
+
+	printf("\n");
+
+	return (0);
+}
