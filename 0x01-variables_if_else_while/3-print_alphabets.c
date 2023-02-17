b0bc9882ea97e18217d161eb202230c176db2af9
Manu254rce <straussmoabit@gmail.com>
2023-02-17 11:05:02 -0800

    C variables and control flow

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,2 +1,26 @@
 # include <stdio.h>
 # include <stdlib.h>
+
+/**
+ * main - Program to print lowercase and uppercase alphabets
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; ++i)
+	{
+		putchar(i);
+	}
+
+	for (i = 'a'; i <= 'z'; ++i)
+	{
+		putchar(toupper(i));
+	}
+
+	putchar('\n');
+
+	return (0);
+}
