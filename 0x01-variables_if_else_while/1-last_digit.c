ac058efe90e8c0a63445671b9799fd5690520dd4
Manu254rce <straussmoabit@gmail.com>
2023-02-17 08:47:32 -0800

    C variables and control flow

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,35 @@
+# include <stdlib.h>
+# include <time.h>
+# include <stdio.h>
+
+/**
+ * main - Program to print condition of last random number
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	int lastDigit = n % 10;
+
+	if (lastDigit > 5)
+	{
+		printf("Last digit of n is %d and is greater than 5\n", lastDigit);
+	}
+
+	else if (lastDigit == 0)
+	{
+		printf("Last digit of n is %d and is 0", lastDigit);
+	}
+
+	else if (lastDigit < 6 && n != 0)
+	{
+		printf("Last digit of n is %d and is less than 6 and not 0\n", lastDigit);
+	}
+
+	return (0);
+}
