10eae9ced4802072e95a37a8e7e4ed830c0f0c6b
Manu254rce <straussmoabit@gmail.com>
2023-02-17 11:36:26 -0800

    C variables and control flow

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+# include <stdio.h>
+# include <stdlib.h>
+
+/**
+ * main - Program to print decimal digits with putchar
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 10; i++)
+	{
+		putchar(i);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
