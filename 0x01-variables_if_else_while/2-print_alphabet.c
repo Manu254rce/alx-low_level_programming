313dc50b41f4709391e81eef0fd8635277388b11
Manu254rce <straussmoabit@gmail.com>
2023-02-17 09:33:13 -0800

    C variables and control flow

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+# include <stdlib.h>
+# include <stdio.h>
+
+/**
+ * main - Program to change alphabet chars to lowercase
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 97; i<= 122; i++)
+	{
+		printf("%c", i);
+	}
+
+	return (0);
+}
