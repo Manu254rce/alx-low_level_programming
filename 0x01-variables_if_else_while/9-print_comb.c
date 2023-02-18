3441af1bf6aa54725f2d1ca79fd3b33206bdd41f
Manu254rce <straussmoabit@gmail.com>
2023-02-17 20:33:44 -0800

    C variables and control flow

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,27 @@
+# include <stdio.h>
+# include <stdlib.h>
+
+/**
+ * main -Program to print all possible combinations of decimal digits
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 48; i <= 58; i++)
+	{
+		putchar(i);
+
+		if (i < 57)
+		{
+			putchar(44);
+			putchar(32);
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
