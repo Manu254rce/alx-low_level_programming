fe4d33cbf210b74de03ed190ba4c85dfdf379eae
Manu254rce <straussmoabit@gmail.com>
2023-02-18 01:30:41 -0800

    C varibles and control flow

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,28 @@
+# include <stdio.h>
+# include <stdlib.h>
+
+/**
+ * main - Program to print all possible combinations of two digits
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int num[2];
+
+	int i;
+	int j;
+
+	for (i = 0; i <= 9; ++i)
+	{
+		for (j = 0; j <= 9; ++j)
+		{
+			if (i != j && j != i)
+			{
+				putchar(num[i], num[j]);
+			}
+		}
+	}
+
+	return (0);
+}
