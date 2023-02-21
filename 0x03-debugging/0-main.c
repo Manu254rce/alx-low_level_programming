c22534518ffc2106146d692bcdada97ddce21d8c
Manu254rce <straussmoabit@gmail.com>
2023-02-21 09:04:37 -0800

    C debugging tasks

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,18 @@
+# include <stdio.h>
+# include <stdlib.h>
+# include "main.h"
+
+/**
+ * main - Entry point of code
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
