89f4aa9ebbc758614830fc84a80e13cb22156ff6
Manu254rce <straussmoabit@gmail.com>
2023-02-21 14:20:21 -0800

    C looping and functions

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,26 @@
+# include "main.h"
+
+/**
+ * print_to_98-Function to print to 98 from any starting number
+ * Return: Always 0
+ */
+
+void print_to_98(int n)
+{
+	int i;
+
+	while ( i = n; i <= '9'; i++ )
+	{
+		for (j = n; j <= '8', j++ )
+		{
+			_putchar(i);
+			_putchar(j);
+
+			if (i < 98)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+	}
+}
