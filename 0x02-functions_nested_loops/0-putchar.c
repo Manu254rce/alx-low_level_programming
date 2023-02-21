614f54e39581ad6fb3bd0cc2d3a91f98317f0881
Manu254rce <straussmoabit@gmail.com>
2023-02-21 12:50:42 -0800

    C looping and functions

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -8,13 +8,13 @@
 int main(void)
 {
 	int i;
-	int c[8] = {95,112,117,116,99,104,97,114};
+	int c[8] = {95, 112, 117, 116, 99, 104, 97, 114};
 
 	for (i = 0; i < 8; i++)
 	{
 		_putchar(c[i]);
 	}
-	
+
 	putchar('\n');
 
 	return (0);
