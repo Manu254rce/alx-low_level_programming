5d08a029d63f6014f1f2b13494d6b31f7f9fd10f
Manu254rce <straussmoabit@gmail.com>
2023-02-23 10:46:55 -0800

    C functions and loops

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,27 @@
+# include "main.h"
+
+/**
+ * print_most_numbers- Program to print all numbers except 2 and 4
+ * Return: Always 0
+ */
+
+void print_most_numbers(void)
+{
+	int c = 48;
+
+	while (c < 58)
+	{
+		if (c == 50 || c == 52)
+		{
+			continue;
+		}
+
+		else
+		{
+			_putchar(c);
+		}
+		c++;
+	}
+
+	_putchar('\n');
+}
