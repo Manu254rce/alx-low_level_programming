8cdee146189507664159f40cf79ef93db5a60dcb
Manu254rce <straussmoabit@gmail.com>
2023-02-22 21:10:47 -0800

    C functions and nested loops part 2

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+# include "main.h"
+
+/**
+ * _isupper- Prints the uppercase letter
+ * Return: Always 0.
+ */
+
+int _isupper(int c)
+{
+	if (isupper(c))
+	{
+		return(1);
+	}
+	else
+	{
+		return(0);
+	}
+}
