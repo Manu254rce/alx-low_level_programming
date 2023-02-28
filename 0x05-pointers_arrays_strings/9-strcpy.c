2f8c5252e47b320a78264e6f0cb304640826b369
Manu254rce <straussmoabit@gmail.com>
2023-02-28 08:24:41 -0800

    C arrays, pointers and functions

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,16 @@
+# include "main.h"
+# include <string.h>
+
+/**
+ * *_strcpy- Program function to copy a string pointer
+ * @dest: variable to store copied string
+ * @src: variable to store source srting
+ * Return: Always 0
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	char *strcpy(*dest, *src);
+
+	return (*dest);
+}
