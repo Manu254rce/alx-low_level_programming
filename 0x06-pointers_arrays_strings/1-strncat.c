b434049407369290a473cf1822b25d9db7789c97
Manu254rce <straussmoabit@gmail.com>
2023-03-02 13:33:15 -0800

    C pointers and arrays

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,26 @@
+# include "main.h"
+
+/**
+ * *_strncat - program to concatenate strings with strncat
+ * @src: Source string
+ * @dest: destination string
+ * @n: iterator variable
+ * Return: Always 0
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, j;
+
+	for (i = 0; dest[i] != '\0'; i++)
+		;
+
+	for (j = 0; src[j] != '\0' && n > 0; i++, n--, j++)
+	{
+		dest[i] = src[j];
+	}
+
+	dest[i] = '\0';
+
+	return (dest);
+}
