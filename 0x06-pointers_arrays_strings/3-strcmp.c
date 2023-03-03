fdad66065f9cb6d3776df07d573e53cf99cd5bd6
Manu254rce <straussmoabit@gmail.com>
2023-03-02 18:25:52 -0800

    C pointers ad arrays

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,24 @@
+# include "main.h"
+
+/**
+ * _strcmp - Program to compare two srtings
+ * @s1: string1
+ * @s2: string2
+ * Return: 'diff'
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int i;
+
+	for (i = 0; s1[i] != '\0' || s2[i] != '\0'; ++i)
+	{
+		if (s1[i] != s2[i])
+		{
+			diff = s1[i] - s2[i];
+			return (diff);
+		}
+	}
+
+	return (0);
+}
