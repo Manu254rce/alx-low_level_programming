05c3fb967a6201c9f07f64839ffc02bce50585d1
Manu254rce <straussmoabit@gmail.com>
2023-03-08 11:18:07 -0800

    C recursion

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -12,13 +12,14 @@ int _strlen_recursion(char *s)
 
 	if (*s != '\0')
 	{
-		count++;
-		_strlen_recursion(s + 1);
-	}
-	else
-	{
-		return (0);
+		count ++;
+		_strlen_recursion(++s);
 	}
 
-	return (count);
+	else
+	{
+		return (count);
+	}
+
+	return ('\0');
 }
