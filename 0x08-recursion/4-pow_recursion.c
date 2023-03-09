9b4f579daa1e12a41f4e80b620925d36f757abd6
Manu254rce <straussmoabit@gmail.com>
2023-03-08 22:35:57 -0800

    C recursion

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -16,7 +16,12 @@ int _pow_recursion(int x, int y)
 		return (1);
 	}
 
-	power = x * (_pow_recursion(x, y-1));
-	
+	else if (y < 0)
+	{
+		return (-1);
+	}
+
+	power = x * (_pow_recursion(x, y - 1));
+
 	return (power);
 }
