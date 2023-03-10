31e66c51e1d9c9d8e5154e409f2213ba555a3482
Manu254rce <straussmoabit@gmail.com>
2023-03-10 13:20:43 -0800

    C static library creation

---
+++ 0x09-static_libraries/3-islower.c
@@ -0,0 +1,20 @@
+# include "main.h"
+# include <ctype.h>
+
+/**
+ * _islower-Function that checks for lowercase letters
+ * Return: Always 0 (Success)
+ */
+
+int _islower(int c)
+{
+	if (islower(c))
+	{
+		return (1);
+	}
+
+	else
+	{
+		return (0);
+	}
+}
