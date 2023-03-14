a1fdf56e48c3e3879e23d596eba99076b6b97d42
Manu254rce <straussmoabit@gmail.com>
2023-03-14 07:57:48 -0700

    C malloc

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -19,7 +19,7 @@ char *_strdup(char *str)
 	for (n = 0; str[n] != '\0'; ++n)
 		;
 
-	str2 =(char*)malloc(n * sizeof(char) + 1);
+	str2 =(char *)malloc(n * sizeof(char) + 1);
 
 	if (str2 == NULL)
 	{
