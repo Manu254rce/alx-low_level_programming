0b300391c02df37ce95ff02756744630638932dd
Manu254rce <straussmoabit@gmail.com>
2023-06-07 15:13:53 -0700

    Power recurrsion in C

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,7 +1,6 @@
 #include "main.h"
 
 /**
- *
  * _sqrt_recursion - returns the natural square root of a number
  * @n: number
  * Return: square root of n
@@ -13,5 +12,21 @@ int _sqrt_recursion(int n)
 		return (-1);
 	if (n == 0)
 		return (0);
-	return (_sqrt_recursion(n, 1));
+	return (sqrt_check(n, 1));
+}
+
+/**
+ * sqrt_check - checks for the square root of c
+ * @c: number o find sqrt
+ * @i: iterator
+ * Return: square root of c
+ */
+
+int sqrt_check(int c, int i)
+{
+	if (c == i * i)
+		return (i);
+	if (c < i * i)
+		return (-1);
+	return (sqrt_check(c, i + 1));
 }
