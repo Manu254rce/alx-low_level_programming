9b10b8e1b9270678a9a3fd931cc40a2f379ec6bd
Manu254rce <straussmoabit@gmail.com>
2023-06-07 13:13:03 -0700

    Power recurrsion in C

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - function that calculates and returns the power of a number
+ * @x: base number
+ * @y: power/ index number
+ * Return: x raised o power y
+ */
+
+int _pow_recursion(int x, in y)
+{
+	if (y == 0)
+		return (-1);
+
+	if (y >= 1)
+		return (x * pow_recursion(x, y - 1));
+	else
+		return (1);
+}
