89daae5a024b66d8063e88918902b2f6fdf09b71
Manu254rce <straussmoabit@gmail.com>
2023-06-09 14:14:20 -0700

    C argc/argv

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#pragma GCC dignostic push
+#pragma GCC diagnostic ignored "-Wunused-parameter"
+
+/**
+ * main - Program that prints out the name of the command used to run it
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: Success
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
