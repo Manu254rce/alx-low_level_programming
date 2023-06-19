fc63110f9e833d40f4172e389c9b6d5581325531
Manu254rce <straussmoabit@gmail.com>
2023-06-19 14:22:05 -0700

    C structure and typedef

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,9 @@
 # ifndef DOG_H
 # define DOG_H
 
+# include <stdio.h>
+# include <stdlib.h>
+
 /**
  * struct dog - The struct for dog
  * @name: name of the dog
@@ -8,11 +11,11 @@
  * @owner: name of dog's owner
  */
 
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-}
+}dog_t;
 
 # endif
