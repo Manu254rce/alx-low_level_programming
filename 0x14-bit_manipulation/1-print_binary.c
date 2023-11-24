09ccf6cdcea39b0d953876a8ed7cddad922d4cb8
Manu254rce <straussmoabit@gmail.com>
2023-11-24 05:24:16 -0800

    task:solved task 1

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,30 @@
+# include "main.h"
+
+/**
+ * print_binary - function that prints binary form
+ * of a number
+ * @n: ineger value
+ * Reurn: binary value of he number
+ */
+
+void print_binary(unsigned long int n)
+{
+	int size = sizeof(unsigned long int) * 8;
+
+	int lead = 1;
+	int i, bit;
+
+	for (i = size - 1; i >= 0; i--)
+	{
+		bit = (n >> i) & 1;
+
+		if (bit || !leadingZeros)
+		{
+			putchar(bit ? '1' : '0');
+			lead = 0;
+		}
+	}
+
+	if (leadingZeros)
+		putchar('0');
+}
