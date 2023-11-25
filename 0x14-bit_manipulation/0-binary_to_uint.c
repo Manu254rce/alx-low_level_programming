93feb691d4dc3b862dac148bebdd37c016b3ed70
Manu254rce <straussmoabit@gmail.com>
2023-11-24 17:24:55 -0800

    task: sorted task 0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -9,28 +9,22 @@
 
 unsigned int binary_to_uint(const char *b)
 {
-
-	unsigned int res, len;
-	int i;
+	unsigned int i, len, num;
 
 	if (b == NULL)
 		return (0);
 
-	res = 0;
-
 	len = strlen(b);
+	num = 0;
 
-	for (i = 0; i < len; ++i)
+	for (i = 0; i < len; i++)
 	{
-		if (b[i] == '1')
-			res = (res << 1) | 1;
-
-		else if (b[i] == '0')
-			res = (res << 1);
-
-		else
+		if (b[i] != '0' && b[i] != '1')
 			return (0);
+
+		num <<= 1;
+		num += b[i] - '0';
 	}
 
-	return (res);
+	return (num);
 }
