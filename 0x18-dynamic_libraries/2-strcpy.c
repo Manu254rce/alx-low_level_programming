edb8a3dea75f5b6f0d394b19acd4aef926977501
Manu254rce <straussmoabit@gmail.com>
2024-01-15 23:31:36 +0300

    Completed task 0

--- 0x18-dynamic_libraries/2-strcpy.c
+++ 0x18-dynamic_libraries/2-strcpy.c
@@ -1,27 +1,15 @@
 # include "main.h"
 
 /**
- * _strncpy - program to copy string
+ * _strcpy - program to copy string
  * @src: source string
  * @dest: destination string
  * @n: number of chars to copy
  * Return: 'dest'
  */
 
-char *_strncpy(char *dest, char *src, int n)
+char *_strcpy(char *dest, char *src)
 {
-	int i;
-
-	for (i = 0; i < n && src[i] != '\0'; i++)
-	{
-		dest[i] = src[i];
-	}
-
-	for (; n > i; i++)
-	{
-		dest[i] = '\0';
-	}
-
 	return (dest);
 }
 
---
+++ 0x18-dynamic_libraries/_strlen.c
@@ -0,0 +1,17 @@
+# include "main.h"
+
+/**
+ * _strlen - function that prints the length of a string
+ * @s: the string character
+ * Return: the integer length of string
+ */
+
+int _strlen(char *s)
+{
+	ssize_t i;
+
+	for (i = 0; i != '\0'; ++i)
+		i += 1;
+
+	return (i);
+}
